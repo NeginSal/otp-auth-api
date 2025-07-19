ca33f800cd5dd307a3fec2e4f206d2082e172d03
NeginSal <neginsaljooghi@gmail.com>
2025-07-19 17:46:51 +0330

    feat: add main.go and .env setup for MongoDB Atlas and Gin server

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello,World !")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Failed to load .env file")
+	}
+
+	client := config.ConnectDB()
+	defer client.Disconnect(nil)
+
+	routes.SetupRoutes(router, client)
+
+	port := config.GetEnv("PORT", "8080")
+	log.Println("Server running on port " + port)
+	err = router.Run(":" + port)
+	if err != nil {
+		log.Fatal("Failed to start server")
+	}
 }
